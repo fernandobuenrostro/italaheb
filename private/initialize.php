0adc618b3cc49c853d7bbb5fc390668e03fefb8c
fernandobuenrostro <67055289+fernandobuenrostro@users.noreply.github.com>
2020-10-02 20:01:21 -0500

    Update initialize.php
    Removed security vulnerability

--- private/initialize.php
+++ private/initialize.php
@@ -13,8 +13,6 @@
   // * Do not need to include the domain
   // * Use same document root as webserver
   // * Can set a hardcoded value:
-  // define("WWW_ROOT", '/~fernandobuenrostro/itala/public');
-  // define("WWW_ROOT", '');
   // * Can dynamically find everything in URL up to "/public"
   $public_end = strpos($_SERVER['SCRIPT_NAME'], '/public') + 7;
   $doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
